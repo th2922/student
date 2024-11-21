29854a7d054437eb8944edbd10f9202bdfd8a8e4
Yashnandi74 <yashyashwanth7447@gmail.com>
2024-11-21 22:44:48 +0530

    yash adding

--- controllers/blog_controller.js
+++ controllers/blog_controller.js
@@ -1,9 +1,14 @@
 import blog_model from "../modules/blog_model.js"
 
 export const blog = async( req , res, next)=> {
-    return res.status(200).json({ message : "there is no blog yet"})
+    let allblog;
+    try{
+         allblog= await blog_model.find();
+    }catch(err){
+        console.log(`---------------------> ${err}`);
+    }
+    return res.status(200).json({allblog});
 }
-
 export const addblog =async( req ,res, next )=>{
     const {titel, descreption, image,dateANdTime} =req.body;
 
@@ -15,4 +20,42 @@ export const addblog =async( req ,res, next )=>{
         console.log(`---------------------> ${err}`);
      }
      res.status(200).json({message:"your blog is saved"});
+}
+export const getBlogbyID= async(req,res,next)=>{
+    const blogID =req.params.id;
+    let blog;
+    try{
+        blog = await blog_model.findById(blogID);
+    }catch(err){
+        return res.status(500).json({message:`---------------------> ${err}`});
+    }
+    return res.status(200).json({blog});
+}
+export const updateblog= async(req,res,next)=>{
+    const blogID =req.params.id;
+    const {titel, descreption, image,dateANdTime} =req.body;
+    let blog;
+    try{
+         await blog_model.findByIdAndUpdate(blogID,{
+            titel, 
+            descreption, 
+            image,
+            dateANdTime,
+         }
+        );
+    }catch(err){
+        return res.status(500).json({message:`---------------------> ${err}`});
+    }
+    return res.status(200).json({message:`blog is updated`});
+}
+
+export const deleteBlog =async (req,res,next)=>{
+    const blogID =req.params.id;
+
+    try{
+        await blog_model.findByIdAndDelete(blogID);
+    }catch(err){
+        return res.status(500).json({message:`---------------------> ${err}`});
+    }
+    return res.status(200).json({message:`deleted succelfuly`});
 }
--- routes/blog_route.js
+++ routes/blog_route.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { addblog, blog } from "../controllers/blog_controller.js";
+import { addblog, 
+        blog,
+        deleteBlog,
+         getBlogbyID,  
+         updateblog } from "../controllers/blog_controller.js";
 
 const blogRoute = express.Router();
 
-blogRoute.get("/",blog);
+blogRoute.get("/allblog",blog);
 blogRoute.post("/addblog",addblog);
+blogRoute.get("/:id",getBlogbyID);
+blogRoute.put("/update/:id",updateblog);
+blogRoute.delete("/delete/:id",deleteBlog);
+
 
 export default blogRoute;
